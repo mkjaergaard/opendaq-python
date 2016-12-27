e25cabba4db27ecce7e51c6bdf03269005c7fd0c
joserb <joserb@ingen10.com>
2016-12-27 19:02:28 +0100

    added opendaq-N support

--- opendaq/model.py
+++ opendaq/model.py
@@ -44,14 +44,14 @@ class DAQModel(object):
         return list(range(0, self.dac_slots))
 
     def adc_coef_range(self, flag):
-        return list(range(self.dac_slots, self.dac_slots+self.adc_slots))
+        return list(range(self.dac_slots, self.dac_slots + self.adc_slots))
 
     def check_valid_pio(self, number):
         if not (1 <= number <= self.pio_slots):
             raise ValueError("PIO number out of range")
 
     def check_valid_port(self, value):
-        if not (0 <= value < 2**(self.pio_slots+1)):
+        if not (0 <= value < 2**(self.pio_slots + 1)):
             raise ValueError("Port number out of range")
 
     def check_valid_dac_value(self, volts):
@@ -90,8 +90,8 @@ class DAQModel(object):
         offset = self.dac_offsets[number]
 
         raw = int(round((volts-offset)/(corr*self.dac_base_gain)))
-        #print self.dac_gains, self.dac_offsets
-        #print raw,"= (",volts,"-",offset,"/(",corr,"*",self.dac_base_gain,")"
+        # print self.dac_gains, self.dac_offsets
+        # print raw,"= (",volts,"-",offset,"/(",corr,"*",self.dac_base_gain,")"
         return max(-32768, min(raw, 32767))  # clamp value
 
 
@@ -106,8 +106,8 @@ class ModelM(DAQModel):
         self.adc_slots = 13
         self.adc_gains = []
         self.adc_offsets = []
-        self.adc_base_ampli = [1./3, 1, 2, 10, 100]
-        self.adc_base_gain = 32768/4.096
+        self.adc_base_ampli = [1. / 3, 1, 2, 10, 100]
+        self.adc_base_gain = 32768 / 4.096
         self.min_adc_value = -4.096
         self.max_adc_value = 4.095
 
@@ -117,13 +117,13 @@ class ModelM(DAQModel):
         self.dac_slots = 1
         self.dac_gains = []
         self.dac_offsets = []
-        self.dac_base_gain = 4.096/32768.
+        self.dac_base_gain = 4.096 / 32768.
         self.min_dac_value = -4.096
         self.max_dac_value = 4.095
 
     @property
     def serial_str(self):
-        return "ODM08%03d6" % self.serial
+        return "ODM08%03d7" % self.serial
 
     def raw_to_volts(self, raw, gain_id, pinput, ninput=0):
         """
@@ -133,7 +133,7 @@ class ModelM(DAQModel):
             raw: Value to convert to volts
             gain_id: ID of the analog configuration setup
         """
-        base_gain = 1./(self.adc_base_gain * self.adc_base_ampli[gain_id])
+        base_gain = 1. / (self.adc_base_gain * self.adc_base_ampli[gain_id])
 
         adc_chp_slot = pinput-1
         adc_gain_slot = len(self.pinput_range)+gain_id
@@ -144,16 +144,7 @@ class ModelM(DAQModel):
                   self.adc_base_ampli[gain_id] +
                   self.adc_offsets[adc_gain_slot])
 
-        """
-        print raw
-        print adc_chp_slot, adc_gain_slot
-        print self.adc_gains[adc_chp_slot],self.adc_gains[adc_gain_slot]
-        print self.adc_offsets[adc_chp_slot] * self.adc_base_ampli[gain_id]
-        print self.adc_offsets[adc_gain_slot]
-        print "\n",1./gain
-        print offset
-        """
-        return round((raw - offset)*base_gain*gain, 5)
+        return round((raw - offset) * base_gain * gain, 5)
 
 
 class ModelS(DAQModel):
@@ -168,7 +159,7 @@ class ModelS(DAQModel):
         self.adc_gains = []
         self.adc_offsets = []
         self.adc_base_ampli = [1, 2, 4, 5, 8, 10, 16, 20]
-        self.adc_base_gain = 32768/12.0
+        self.adc_base_gain = 32768 / 12.0
         self.min_adc_value = -12
         self.max_adc_value = 12
 
@@ -178,13 +169,13 @@ class ModelS(DAQModel):
         self.dac_slots = 1
         self.dac_gains = []
         self.dac_offsets = []
-        self.dac_base_gain = 4.096/32768.
+        self.dac_base_gain = 4.096 / 32768.
         self.min_dac_value = 0
         self.max_dac_value = 4.095
 
     @property
     def serial_str(self):
-        return "ODS08%03d6" % self.serial
+        return "ODS08%03d7" % self.serial
 
     def raw_to_volts(self, raw, gain_id, pinput, ninput=0):
         """
@@ -200,8 +191,7 @@ class ModelS(DAQModel):
         base_gain = 1./(self.adc_base_gain * self.adc_base_ampli[gain_id])
         gain = self.adc_gains[adc_chp_slot]
         offset = self.adc_offsets[adc_chp_slot]
-        #print raw, gain, offset, round(raw*base_gain,4)
-        return round((raw - offset)*base_gain/gain, 4)
+        return round((raw - offset) * base_gain / gain, 4)
 
     def check_valid_adc_settings(self, pinput, ninput, xgain):
         if pinput not in self.pinput_range:
@@ -216,15 +206,64 @@ class ModelS(DAQModel):
 
     def adc_coef_range(self, flag):
         if flag == 'SE':
-            return list(range(self.dac_slots, self.dac_slots+self.adc_slots/2))
+            return list(range(self.dac_slots, self.dac_slots+self.adc_slots / 2))
         elif flag == 'DE':
-            return list(range(self.dac_slots+self.adc_slots/2,
-                         self.dac_slots+self.adc_slots))
+            return list(range(self.dac_slots + self.adc_slots / 2,
+                         self.dac_slots + self.adc_slots))
         elif flag == 'ALL':
             return list(range(self.dac_slots, self.dac_slots+self.adc_slots))
         else:
             raise ValueError("Invalid flag")
 
+
+class ModelN(DAQModel):
+    _id = 3
+
+    def __init__(self, fw_ver, serial):
+        DAQModel.__init__(self, fw_ver, serial)
+        self.model_str = "[N]"
+        self.pio_slots = 6
+
+        self.adc_slots = 16
+        self.adc_gains = []
+        self.adc_offsets = []
+        self.adc_base_ampli = [1, 2, 4, 5, 8, 10, 16, 32]
+        self.adc_base_gain = 32768 / 12.288   # 6 * 2.048
+        self.min_adc_value = -4.096
+        self.max_adc_value = 4.095
+
+        self.pinput_range = list(range(1, 9))
+        self.ninput_range = list(range(0, 9))
+
+        self.dac_slots = 1
+        self.dac_gains = []
+        self.dac_offsets = []
+        self.dac_base_gain = 4.096/32768.
+        self.min_dac_value = -4.096
+        self.max_dac_value = 4.095
+
+    @property
+    def serial_str(self):
+        return "ODN08%03d7" % self.serial
+
+    def raw_to_volts(self, raw, gain_id, pinput, ninput=0):
+        """
+        Convert a raw value to a value in volts.
+
+        Args:
+            raw: Value to convert to volts
+            gain_id: ID of the analog configuration setup
+        """
+        base_gain = 1. / (self.adc_base_gain * self.adc_base_ampli[gain_id])
+
+        adc_chp_slot = pinput - 1
+
+        gain = 1. / (self.adc_gains[adc_chp_slot])
+        offset = (self.adc_offsets[adc_chp_slot] *
+                  self.adc_base_ampli[gain_id] +
+                  self.adc_offsets[adc_chp_slot + 8])
+
+        return round((raw - offset) * base_gain * gain, 5)
 
 class ModelTP8(DAQModel):
     _id = 10
